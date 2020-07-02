3939c05a9876f193a37020695131f232653a520f
Meianna Oeser <meianna.oeser@gmail.com>
2020-07-01 17:38:04 -0700

    wrote add department and add employee functionality

--- app.js
+++ app.js
@@ -25,14 +25,14 @@ function display() {
       {
         type: "rawlist",
         choices: [
+          "add department",
           "add role",
           "add employee",
-          "add department",
-          "display all departments",
-          "display all roles",
-          "display all employees",
-          "update role",
+          "view all departments",
+          "view all roles",
+          "view all employees",
           "update departments",
+          "update role",
           "update employees",
           "exit application",
         ],
@@ -41,12 +41,71 @@ function display() {
     ])
     .then(function (userChoice) {
       switch (userChoice.navigation) {
+        case "add department":
+          addDepartment();
+          break;
+        default:
+          connection.end;
+      }
+      switch (userChoice.navigation) {
         case "add role":
           addRole();
           break;
         default:
           connection.end;
       }
+      switch (userChoice.navigation) {
+        case "add employee":
+          addEmployee();
+          break;
+        default:
+          connection.end;
+      }
+      switch (userChoice.navigation) {
+        case "view all departments":
+          viewDepartments();
+          break;
+        default:
+          connection.end;
+      }
+      switch (userChoice.navigation) {
+        case "view all roles":
+          viewRoles();
+          break;
+        default:
+          connection.end;
+      }
+      switch (userChoice.navigation) {
+        case "view all employees":
+          viewEmployees();
+          break;
+        default:
+          connection.end;
+      }
+    });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+      },
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT INTO department (name) VALUES (?);",
+        [res.name],
+        function (err, data) {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+          console.log("department added");
+          display();
+        }
+      );
     });
 }
 
@@ -62,21 +121,56 @@ function addRole() {
         name: "salary",
       },
       {
-        type: "list",
+        type: "input",
         name: "department_id",
-        choices: [1, 2, 3],
       },
     ])
-    .then(function (userData) {
+    .then(function (res) {
       connection.query(
         "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);",
-        [userData.title, userData.salary, userData.department_id],
+        [res.title, res.salary, res.department_id],
         function (err, data) {
           if (err) {
             console.log(err);
             throw err;
           }
-          console.log("role updated");
+          console.log("role added");
+          display();
+        }
+      );
+    });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+      },
+      {
+        type: "input",
+        name: "last_name",
+      },
+      {
+        type: "input",
+        name: "role_id",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+      },
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);",
+        [res.first_name, res.last_name, res.role_id, res.manager_id],
+        function (err, data) {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+          console.log("employee added");
           display();
         }
       );
